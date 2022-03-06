561b7bac70d07fcfa8ee7dedf9a53c67dbcdd080
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-03-06 14:22:36 +0600

    normal change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function Welcome(props) {
+  return <h1>Mithi, {props.name}</h1>;
+}
+
+function App() {
+  return (
+    <div>
+      <Welcome name="i love you" />
+      <Welcome name="Do you Love me " />
+      <Welcome name="take Love LojjaBoti" />
+    </div>
+  );
+}
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
+  <App />,
   document.getElementById('root')
 );
