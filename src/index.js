84bee13cc2db8362fb4a285f87e9c392381dc96b
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-03-07 23:25:51 +0600

    clock 2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-setInterval(() => {
-  const element = (
-    <h1 className='heading'>
-      <span className='text'>Time: {new Date().toLocaleTimeString()}</span>
-    </h1>
+function Clock(props) {
+  return (
+    <div>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
   );
-  ReactDOM.render(element, document.getElementById('root'));
-}, 1000);
+}
+
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
