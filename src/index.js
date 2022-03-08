62200a86e0889684e3a4e5deaf11808350ccaf1a
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-03-08 08:49:15 +0600

    3rd clock

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Clock(props) {
-  return (
-    <div>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
-}
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()};
+  }
+  componentDidMount(){
+    this.timerID = setInterval(() => {
+      this.tick()
+    }, 1000);
+  }
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+  tick(){
+  this.setState({
+    date: new Date()
+  });
+  }
 
-function tick() {
+  render(){
+    return (
+      <div>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
   ReactDOM.render(
-    <Clock date={new Date()} />,
+    <Clock />,
     document.getElementById('root')
   );
-}
-
-setInterval(tick, 1000);
