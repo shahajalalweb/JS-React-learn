a23042cbf71db82004c19b8a0d12abe149193a48
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-03-09 00:22:36 +0600

    on off button

--- src/index.js
+++ src/index.js
@@ -1,34 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Clock extends React.Component {
+class Toggle  extends React.Component {
   constructor(props){
     super(props);
-    this.state = {date: new Date()};
+    this.state = {isToggleOn: true};
+    this.handleClick = this.handleClick.bind(this);
   }
-  componentDidMount(){
-    this.timerID = setInterval(() => {
-      this.tick()
-    }, 1000);
+  handleClick(){
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
   }
-  componentWillUnmount(){
-    clearInterval(this.timerID);
-  }
-  tick(){
-  this.setState({
-    date: new Date()
-  });
-  }
-
   render(){
     return (
-      <div>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
     );
   }
 }
-  ReactDOM.render(
-    <Clock />,
-    document.getElementById('root')
-  );
+
+
+ReactDOM.render (
+  <Toggle  />,
+  document.getElementById('root')
+);
