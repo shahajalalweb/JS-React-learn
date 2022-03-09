210096f491c4dd3eae106fd4c9968be9bd229f99
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-03-09 08:45:42 +0600

    oder folder clock add

--- src/index.js
+++ src/index.js
@@ -1,34 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
-class Clock extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {date: new Date()};
-  }
-  componentDidMount(){
-    this.timerID = setInterval(() => {
-      this.tick()
-    }, 1000);
-  }
-  componentWillUnmount(){
-    clearInterval(this.timerID);
-  }
-  tick(){
-  this.setState({
-    date: new Date()
-  });
-  }
+ReactDOM.render (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-  render(){
-    return (
-      <div>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
-  ReactDOM.render(
-    <Clock />,
-    document.getElementById('root')
-  );
