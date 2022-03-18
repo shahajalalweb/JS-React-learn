7480924255f5a59bdfd5925a3e2a18df0de873ea
shahajalalweb <shahajalalbadsha5@gmail.com>
2022-03-19 00:52:09 +0600

    fist context added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,60 @@
-import Calculator from "./component/Calculator";
+import React from 'react';
+import ThemeButton from './practice/button';
+import { ThemeContext, themes } from './practice/theme';
 
-function App() {
-  return <Calculator />
+function Toolbar(props) {
+  return (
+    <ThemeButton onClick={props.changeTheme}>
+      Change Theme
+    </ThemeButton>
+  );
 }
 
-export default App;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      theme: themes.dark,
+    };
+
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme: 
+          state.theme === themes.dark ? themes.light : themes.dark,
+      }));
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <Toolbar changeTheme={this.toggleTheme} />
+        </ThemeContext.Provider>
+        <section>
+          <ThemeButton />
+        </section>
+      </div>
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
